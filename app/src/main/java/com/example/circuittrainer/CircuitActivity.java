26e98d3da261d7a860aa25fae188a4e8ca574f83
Justin Thomas <jthoma59@uic.edu>
2020-04-18 10:44:49 -0500

    redesign for CircuitActivity

--- app/src/main/java/com/example/circuittrainer/CircuitActivity.java
+++ app/src/main/java/com/example/circuittrainer/CircuitActivity.java
@@ -4,36 +4,67 @@
 
 package com.example.circuittrainer;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.HashMap;
 
 public class CircuitActivity extends AppCompatActivity {
 
-    ListView builderList;
-    Button saveButton;
-    Button startButton;
-    ArrayAdapter<String> arrayAdapter;
-    String circuit_builder_items[];
+    ListView exerciseList;
+    Button add_exercise;
+    Button add_rest;
+    Button add_set;
+    Button sub_set;
+    Button add_warning;
+    Button sub_warning;
+    Button save_button;
+    Button start_button;
+    TextView num_sets;
+    TextView num_warning;
+    int sets;
+    int warning;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.circuit_activity);
+        setContentView(R.layout.circuit_layout);
 
-        builderList = (ListView) findViewById(R.id.builder_list);
-        saveButton = (Button) findViewById(R.id.save_button);
-        startButton = (Button) findViewById(R.id.start_button);
-        circuit_builder_items = getResources().getStringArray(R.array.circuit_builder_items);
+        //  get references to UI elements
+        exerciseList = (ListView) findViewById(R.id.exercise_list);
+        add_exercise = (Button) findViewById(R.id.add_exercise);
+        add_rest = (Button) findViewById(R.id.add_rest);
+        add_set = (Button) findViewById(R.id.add_set);
+        sub_set = (Button) findViewById(R.id.sub_set);
+        add_warning = (Button) findViewById(R.id.add_warning);
+        sub_warning = (Button) findViewById(R.id.sub_warning);
+        save_button = (Button) findViewById(R.id.save_button);
+        start_button = (Button) findViewById(R.id.start_button);
+        num_sets = (TextView) findViewById(R.id.num_sets);
+        num_warning = (TextView) findViewById(R.id.num_warning);
 
-        //  define adapter to populate the ListView with
-        arrayAdapter = new ArrayAdapter<String>(this, R.layout.list_item, circuit_builder_items);
+        //  set initial values for sets and warnings
+        sets = 1;
+        warning = 0;
+        num_sets.setText(""+sets);
+        num_warning.setText(""+warning);
 
-        //  populate ListView with the appropriate items
-        builderList.setAdapter(arrayAdapter);
-        
+
     }
+
+
+
+
+
 }
