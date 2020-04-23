75e1264123600460896bf5dbfea14f7fe2d0abb8
Justin Thomas <jthoma59@uic.edu>
2020-04-22 20:37:42 -0500

    bottom half of layout for timer

--- app/src/main/java/com/example/circuittrainer/TimerActivity.java
+++ app/src/main/java/com/example/circuittrainer/TimerActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.content.Intent;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -12,8 +15,12 @@ public class TimerActivity extends AppCompatActivity {
 
     ArrayList<String> namesQueue;                //  queue will contain user entered circuit exercises
     ArrayList<Integer> timesQueue;                   //  queue will contain user entered circuit times
+    ArrayAdapter<String> adapter;               //  will place namesQueue inside list view
+    ListView timerList;                         //  contains names of exercises/rest
+    TextView numSetsText;                       //  displays the number of sets
     int sets;
     int warning;
+    int currSet;                                //  set user is currently on
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +33,18 @@ public class TimerActivity extends AppCompatActivity {
         warning = i.getIntExtra("warning", 0);
         namesQueue = i.getStringArrayListExtra("namesQueue");
         timesQueue = i.getIntegerArrayListExtra("timeQueue");
+        currSet = 1;
 
-        int x=1;
+        timerList = (ListView) findViewById(R.id.timer_list);
+        numSetsText = (TextView) findViewById(R.id.set_text);
+
+        adapter = new ArrayAdapter<String>(this, R.layout.interval_listitem_layout, namesQueue);
+        timerList.setAdapter(adapter);
+
+        numSetsText.setText("Sets: " + currSet + "/" + sets);           //  display 1/sets
+
+
+
+
     }
 }
